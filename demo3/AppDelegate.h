54d8d643c0c06f14fcb0e1e0f41f801a7ac74207
spacetime <jingweiming-sp@outlook.com>
2016-10-28 17:06:30 +0800

    Initial Commit

---
+++ demo3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo3
+//
+//  Created by spacetime on 10/28/16.
+//  Copyright © 2016 spacetime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo3
+//
+//  Created by spacetime on 10/28/16.
+//  Copyright © 2016 spacetime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
